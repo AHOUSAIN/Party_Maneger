8fea2066ec13c943911a58598bcd65129730c2b0
AHOUSAIN <afrahhassancmu@gmail.com>
2012-04-13 17:12:58 +0300

    Initial commit ignoring some files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PartyManeger::Application.config.secret_token = 'ebe84ee8eb29bdca0cbd5c5b2eb58d83e348cad69825e13a85df6c91ba91ba8c80c105608f42880253bd247a1ce2f981639dcd7c28d8b8b91d4c9232c16fac63'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+PartyManeger::Application.config.session_store :cookie_store, key: '_Party_Maneger_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# PartyManeger::Application.config.session_store :active_record_store
