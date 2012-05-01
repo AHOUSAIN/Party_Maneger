428406517899b320707c7bd73e7ff19b46c8d4b3
AHOUSAIN <afrahhassancmu@gmail.com>
2012-05-01 13:59:13 +0300

    updated

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,18 +1,20 @@
 class GuestsController < ApplicationController
-before_filter :authenticate , :only => [:create,:destroy]
+before_filter :authenticate , :only => [:create,:destroy,:index,:show,:index]
   
   def index
      @guests = Guest.paginate(:page => params[:page])
   end
   def new
-    @party = Party.new(params[:party])
+    @guest = Guest.new(params[:party])
   end
   def create
+   
     @party = Party.new(params[:party])
     @guest = @party.guests.build(params[:guest])
     if @guest.save
       redirect_to @guest, :flash => { :success => "Guest created!" }
     else
+      @feed_items = []
       render 'pages/host'
     end
   end
